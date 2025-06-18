cf3cde94a02bc2d59f6a1143848ee24344692c2a
Luis Alberto Mendoza Vargas <sippuden11@outlook.com>
2025-06-18 15:26:01 -0600

    20250618 0325 pm Envio de Correo

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -9,10 +9,13 @@ namespace Portafolio.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IRepositorioDTO _repositorioDTO;
-        public HomeController(ILogger<HomeController> logger,IRepositorioDTO repositorioDTO )
+        private readonly IServicioEmailGmail _servicioEmail;
+        public HomeController(ILogger<HomeController> logger,IRepositorioDTO repositorioDTO ,
+            IServicioEmailGmail servicioEmailGmail)
         {
             _logger = logger;
             _repositorioDTO = repositorioDTO;
+            _servicioEmail = servicioEmailGmail;
         }
 
         public IActionResult Index()
@@ -27,7 +30,21 @@ namespace Portafolio.Controllers
             var proyectos = _repositorioDTO.ObtenerProyectos();
             return View("_Proyectos",proyectos);
         }
-
+        /*Contacto*/
+        public IActionResult Contacto()
+        {
+            return View("_Contacto");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Contacto(ContactoDTO contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                 await _servicioEmail.Enviar(contacto);
+                return View("_Gracias");
+            }
+            return View("_Contacto", contacto);
+        } 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Portafolio/Servicios/ServicioEmailGmail.cs
@@ -0,0 +1,43 @@
+﻿using Portafolio.Models;
+using System.Net.Mail;
+
+namespace Portafolio.Servicios
+{ 
+    public interface IServicioEmailGmail
+    {
+        Task Enviar(ContactoDTO contacto);
+    }
+    public class ServicioEmailGmail:IServicioEmailGmail
+    {
+        private readonly IConfiguration _configuration;
+        public ServicioEmailGmail(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task Enviar(ContactoDTO contacto)
+        {
+            var email = _configuration.GetValue<string>("CONFIGURACIONES_EMAIL:EMAIL");
+            var password = _configuration.GetValue<string>("CONFIGURACIONES_EMAIL:PASSWORD");
+            var smtpServer = _configuration.GetValue<string>("CONFIGURACIONES_EMAIL:HOST");
+            var smtpPort = _configuration.GetValue<int>("CONFIGURACIONES_EMAIL:PUERTO");
+
+            using (var client = new SmtpClient(smtpServer, smtpPort))
+            { 
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+
+                client.Credentials = new System.Net.NetworkCredential(email, password);
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(email),
+                    Subject = "Nuevo contacto desde el portafolio",
+                    Body = $"El cliente: {contacto.Nombre}\nEmail: {contacto.Email} quiere contactarte.\nMensaje: {contacto.Mensaje}",
+                    IsBodyHtml = false,
+                };
+                mailMessage.To.Add(email);
+
+                await client.SendMailAsync(mailMessage);
+            }
+        }
+    }
+}
